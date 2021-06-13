ca0852702387c705a2c3308dbefb8834cb3fd840
azureswastika <azureswastika@gmail.com>
2021-06-13 15:57:28 +0300

    Django settings

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,10 @@ from django.urls.conf import include
 urlpatterns = [
     path("", include("apps.articles.urls")),
     path("", include("apps.users.urls")),
-    path("admin/", admin.site.urls),
-    path("__debug__/", include(debug_toolbar.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("admin/", admin.site.urls),
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
